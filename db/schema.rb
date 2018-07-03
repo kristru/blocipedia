943f5a408738cb64beef76aeda3d95394a0b83c9
kristru <krissafuentes@gmail.com>
2018-07-02 21:17:39 -0600

    add wiki and comments posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180703012755) do
+ActiveRecord::Schema.define(version: 20180703030819) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "wiki_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wiki_id"], name: "index_comments_on_wiki_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +41,16 @@ ActiveRecord::Schema.define(version: 20180703012755) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "wikis", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "private"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wikis_on_user_id"
   end
 
 end
