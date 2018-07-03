9311cc50b6c2fcb1d9fa5c1c6c5eba4d6bcf19f3
kristru <krissafuentes@gmail.com>
2018-07-02 20:31:53 -0600

    add username field for signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+   GET /resource/sign_up
+   def new
+     super
+     attr_accessible :username
+   end
 
   # POST /resource
   # def create
@@ -51,9 +52,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+   end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
