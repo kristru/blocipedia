bb6050e42208b5ab8608569b30d0789ed8e956b6
kristru <krissafuentes@gmail.com>
2018-07-03 20:23:23 -0600

    create wiki methods and views

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,68 @@
+class WikisController < ApplicationController
+  #before_action :require_sign_in, except: :show
+  #before_action :authorize_user, except: [:show, :new, :create]
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki..new(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Woo hoo! A new Wiki."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Whoops. There was an error saving your Wiki. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki = assign_attributes(post_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Whoops. There was an error saving your Wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def delete
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.delete
+      flash[:notice] = "Bye Bye Wiki."
+      redirect_to Wiki.all
+    else
+      flash.now[:alert] = "Stubborn little bugger. Please try again."
+      render :show
+    end
+  end
+
+#anything below this is private and can be accessed through Wikis
+
+  private
+  def wiki_params
+    params.require(:wiki).permit(:title, :body)
+  end
+
+  def authorize_user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :wikis
+  resources :users
+
   devise_for :users, controllers: {
        confirmations: 'confirmations',
        sessions: 'users/sessions'
      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home/index'
+
   root to: 'home#index'
 end
